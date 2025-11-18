e946ac2633ee842eea7c188e408ce6ad0c61c4c3
Aaron Santana <84732450+Santaval@users.noreply.github.com>
2025-11-18 02:20:20 -0600

    feat: Add Analytics component to App for enhanced tracking

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import JsonToToonPage from "./pages/JsonToToonPage";
 import ApiPage from "./pages/ApiPage";
-
+import { Analytics } from '@vercel/analytics/react';
 export default function App() {
-  return <BrowserRouter>
+  return <>
+  <Analytics />
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/json-to-toon" element={<JsonToToonPage />} />
       <Route path="/api" element={<ApiPage />} />
     </Routes>
   </BrowserRouter>;
+  </>
 }
